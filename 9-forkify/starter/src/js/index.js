d23adc34a5824d9c1a6761baed0afdd5f7e05bc4
Yi Zhou <kickccat@hotmail.com>
2018-08-23 01:05:08 +0200

    add test food2fork api

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,17 @@
-// Global app controller
-import num from './test';
+import axios from 'axios';
+// API Key: a3edb4bbf6b6ef169d9d361ca3530bb8
+// API URL: http://food2fork.com/api/search
 
-console.log(`I imported ${num} from another module called test.js!`);
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = 'a3edb4bbf6b6ef169d9d361ca3530bb8';
+    try {
+        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (e) {
+        alert(e);
+    }
+}
+
+getResults('tomato pasta}}');
